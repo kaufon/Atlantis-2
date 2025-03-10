111b0764a87d86add69321c05d4381ea91822ad4
kaufon <kauanfvale@gmail.com>
2025-03-10 02:15:59 -0300

    ♻️refactor: add output to process

--- core/domain/abstracts/Process.ts
+++ core/domain/abstracts/Process.ts
@@ -1,10 +1,12 @@
+import { OutPut } from "@/libs";
 import { Input } from "../../../src/libs/Input";
-import type { IInput, IMenu } from "../../interfaces";
+import type { IInput, IMenu, IOutput } from "../../interfaces";
 
 export abstract class Process {
   protected option!: number;
   protected menu!: IMenu;
   protected input: IInput = new Input();
+  protected output: IOutput = new OutPut();
   protected process!: Process;
   protected execution!: boolean;
   public get Execution(): boolean {
